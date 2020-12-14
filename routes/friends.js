c4a032c2687b92360d25a4de2b40c58be6e53df9
Edouard Pfauwadel <edouard.pfauwadel@gmail.com>
2020-12-14 18:07:04 +0100

    added route and searchbar for friends

--- routes/friends.js
+++ routes/friends.js
@@ -4,7 +4,28 @@ const UserModel = require("./../models/User");
 
 //ROUTERS ALL TO BE REPLACED WITH AJAX/AXIOS, JUST CODED THEM IN ORDER TO BE ABLE TO DO VIEWS/CSS
 router.get("/", async (req, res) => {
-  res.render("friendsAll");
+  const users = await UserModel.find();
+  res.render("friendsAll", { users });
 });
 
+router.get("/search", async (req, res, next) => {
+  // req.body (posted infos)
+  // req.params (variable/dynamique part of a route path)
+  // req.query (access infos from for with get method)
+  try {
+    console.log(req.query); // query strings
+    const exp = new RegExp(req.query.search); // creating a regular expression
+    const matchedUsers = await UserModel.find({ username: { $regex: exp } });
+
+    res.render("friendsAll", { users: matchedUsers });
+  } catch (err) {
+    next(err);
+  }
+
+});
+
+
+
+
 module.exports = router;
+
