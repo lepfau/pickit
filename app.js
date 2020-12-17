3bf6a06779732dcf8f9903001137ec2a7582b7c2
Kate Pavlova <ekaterina.vik.pavlova@gmail.com>
2020-12-17 20:46:38 +0400

    changed mongoURI

--- app.js
+++ app.js
@@ -13,16 +13,16 @@ const UserModel = require("./models/User");
 const flash = require("connect-flash");
 const session = require("express-session");
 
-// mongoose
-//   .connect("mongodb://localhost/pickit", { useNewUrlParser: true })
-//   .then((x) => {
-//     console.log(
-//       `Connected to Mongo! Database name: "${x.connections[0].name}"`
-//     );
-//   })
-//   .catch((err) => {
-//     console.error("Error connecting to mongo", err);
-//   });
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  .then((x) => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo", err);
+  });
 
 const app_name = require("./package.json").name;
 const debug = require("debug")(
